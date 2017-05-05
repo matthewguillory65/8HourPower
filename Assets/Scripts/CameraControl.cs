016b4d851b7497b6e130950d80e9609aa455c1d4
matthewguillory65 <matthew.guillor@ad.aie.edu>
2017-05-05 17:09:15 -0500

    8 hour game
    Making the 8 hour game

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    public Camera camera;
+    private Vector3 offSet;
+    private Vector3 onSet;
+	// Use this for initialization
+	void Start ()
+    {
+        offSet = camera.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //camera.transform.eulerAngles = transform.position + offSet;
+        camera.transform.position = transform.position + offSet;
+        //camera.transform.localEulerAngles = transform.position + offSet;
+        
+    }
+}
---
+++ Assets/Scripts/SphereMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour {
+    Rigidbody rigidBody;
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 moveMent = new Vector3(moveHorizontal, 0.0F, moveVertical);
+        rigidBody.AddForce(moveMent * speed);
+    }
+}
